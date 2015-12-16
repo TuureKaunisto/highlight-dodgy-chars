c20ba370932940bc4ead13197db797cb7b76de9c
Tuure Kaunisto <tuure.kaunisto@gmail.com>
2015-12-16 23:04:40 +0200

    init

---
+++ HighlightDodgyChars.py
@@ -0,0 +1,37 @@
+import sublime, sublime_plugin
+users_whitelist = ''
+
+def plugin_loaded():
+    # Is there a way to get a reference to the plugin instance here?
+    # It would be nice to avoid having to use the global user_whitelist variable.
+    HighlightDodgyChars.getSettings()
+
+class HighlightDodgyChars(sublime_plugin.EventListener): 
+
+    def getSettings():
+        global users_whitelist
+        settings = sublime.load_settings('HighlightDodgyChars.sublime-settings')
+        users_whitelist = settings.get('whitelist_chars').lower()
+        users_whitelist += users_whitelist.upper() # for some reason the sublime.IGNORECASE -flag did not work :(
+
+    def on_modified_async(self, view):
+        self.highlight(view)
+
+    def on_load_async(self, view):
+        # load highlights as soon as the file is opened
+        self.highlight(view)
+
+    def highlight(self, view):
+        highlights = []
+        whitelist = u'\n´\u0009' # allow newline, forward-tick and tabulator
+        needle = '[^[:print:]'+whitelist+users_whitelist+']' # non-printable chars except whitelisted
+
+        # search the view
+        for pos in view.find_all(needle):
+            highlights.append(pos)
+
+        # if something dodgy was found, highlight the dodgy parts
+        if highlights:
+            view.add_regions("zero-width-and-bad-chars", highlights, "invalid", "", sublime.DRAW_SOLID_UNDERLINE)
+        else:
+            view.erase_regions("zero-width-and-bad-chars") 
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Highlight bad non-ascii characters in Sublime Text 3
+
+This plugin highlights non-printable characters excluding whitelisted characters.
+
+By default the Scandinavian special characters and the euro sign: åäöæø€ are whitelisted. The whitelisted characters can be edited in the settings.
+
+`Sublime Text > Preferences > Package Settings > Highlight Dodgy Chars > Settings - User`
+
+## Installing
+
+The easiest way to install this plugin is with the **Package Control plugin** If you don't have Package Control installed, you can find instructions here: [https://packagecontrol.io/installation](https://packagecontrol.io/installation)
+
+Once you've installed Package Control, restart Sublime and open the Command Palette (Command+Shift+p on OS X, Control+Shift+p on Linux/Windows). Type/select "Package Control: Install Package", wait while Package Control fetches the latest package list, then type/select `Highlight Dodgy Chars` when the list appears.
